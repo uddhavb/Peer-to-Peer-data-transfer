7f0b28d8cb947b1520e85a9ed42d4f5dad3f17da
Uddhav Bhosle <ubhosle@ncsu.edu>
2018-11-21 14:28:11 -0500

    fix print ambiguity

--- Client.py
+++ Client.py
@@ -11,9 +11,11 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 7734))
 
 # send some data (in this case a HTTP GET request)
-client.send('GET /index.html HTTP/1.1\r\nHost: {}.{}\r\n\r\n'.format(sld, tld))
+data = 'GET /index.html HTTP/1.1\r\nHost: {}.{}\r\n\r\n'.format(sld, tld)
+data = bytearray(data)
+client.send(data)
 
 # receive the response data (4096 is recommended buffer size)
 response = client.recv(4096)
 
-print response
+print(response)
